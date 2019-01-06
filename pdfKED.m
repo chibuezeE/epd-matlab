30f7461d27639d4e936bd8ebe7629e9ee158efdf
chibuezeE <42462234+chibuezeE@users.noreply.github.com>
2019-01-06 18:54:08 +0000

    Delete pdfKED.m

--- pdfKED.m
+++
@@ -1,4 +0,0 @@
-function [f]=pdfKED(t,alphak)
-% t=linspace(eps,0.9999,1000);
-f=alphak(3)*((alphak(1)-2*alphak(2).*log(t))./t).*exp(alphak(1)*log(t)-alphak(2)*log(t).^2).*(1-exp(alphak(1)*log(t)-alphak(2)*log(t).^2)).^(alphak(3)-1);
-% f(1)=0;
